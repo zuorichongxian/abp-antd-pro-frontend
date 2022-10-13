129b8e47e10430a1ba607ed9e17d3c141a06aae6
ZUORICHONGXIAN\zuorichongxian <x17620108066@gmail.com>
2022-10-13 15:58:33 +0800

    chore(模块): 对脚手架做了些更改

--- config/config.ts
+++ config/config.ts
@@ -6,8 +6,12 @@ import proxy from './proxy';
 import routes from './routes';
 
 const { REACT_APP_ENV } = process.env;
+console.log('REACT_APP_ENV: ', REACT_APP_ENV);
 
 export default defineConfig({
+  define: {
+    REACT_APP_ENV: REACT_APP_ENV || false,
+  },
   /**
    * @name 开启 hash 模式
    * @description 让 build 之后的产物包含 hash 后缀。通常用于增量发布和避免浏览器加载缓存。
--- src/utils/appconst.js
+++ src/utils/appconst.js
@@ -1,16 +1,17 @@
 const AppConsts = {
-    userManagement: {
-        defaultAdminUserName: 'admin',
-    },
-    localization: {
-        defaultLocalizationSourceName: 'Precise',
-    },
-    authorization: {
-        encrptedAuthTokenName: 'enc_auth_token',
-        allAuthorityName: 'precise_all_permission',
-        grantedAuthorityName: 'precise_granted_permission'
-    },
-    appBaseUrl: "http://127.0.0.1:8000",
-    remoteServiceBaseUrl: "http://127.0.0.1:50538/",
+  userManagement: {
+    defaultAdminUserName: 'admin',
+  },
+  localization: {
+    defaultLocalizationSourceName: 'Precise',
+  },
+  authorization: {
+    encrptedAuthTokenName: 'enc_auth_token',
+    allAuthorityName: 'precise_all_permission',
+    grantedAuthorityName: 'precise_granted_permission',
+  },
+  appBaseUrl: 'http://127.0.0.1:8000',
+  // remoteServiceBaseUrl: "http://127.0.0.1:50538/",
+  remoteServiceBaseUrl: API_URL,
 };
 export default AppConsts;
