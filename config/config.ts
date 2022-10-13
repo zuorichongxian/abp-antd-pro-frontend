0d616819f44b9fd17c62660854d55f22e2adaf27
ZUORICHONGXIAN\zuorichongxian <x17620108066@gmail.com>
2022-10-13 17:45:38 +0800

    Revert "chore(模块): 对脚手架做了些更改"
    This reverts commit 64f6f9000196f8734e2fa1946db541954efbc5a8.

--- config/config.ts
+++ config/config.ts
@@ -43,7 +43,6 @@ export default defineConfig({
     // 只有设置为 variable， 才能使用 configProvide 动态设置主色调
     'root-entry-name': 'variable',
   },
-
   /**
    * @name moment 的国际化配置
    * @description 如果对国际化没有要求，打开之后能减少js的包大小
@@ -83,7 +82,6 @@ export default defineConfig({
     locale: true,
     ...defaultSettings,
   },
-
   /**
    * @name 国际化插件
    * @doc https://umijs.org/docs/max/i18n
@@ -95,7 +93,6 @@ export default defineConfig({
     // default true, when it is true, will use `navigator.language` overwrite default
     baseNavigator: true,
   },
-
   /**
    * @name antd 插件
    * @description 内置了 babel import 插件
@@ -130,12 +127,10 @@ export default defineConfig({
       schemaPath: join(__dirname, 'oneapi.json'),
       mock: false,
     },
-
     {
       requestLibPath: "import { request } from '@umijs/max'",
       schemaPath: 'https://gw.alipayobjects.com/os/antfincdn/CA1dOm%2631B/openapi.json',
       projectName: 'swagger',
     },
   ],
-  tailwindcss: {},
 });
